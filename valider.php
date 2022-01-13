7522008b87aee094d0d4af4eacfead1f12f81324
Vitrant Clément <91595309+CVitrant@users.noreply.github.com>
2022-01-13 09:40:52 +0100

    Add files via upload

--- valider.php
+++ valider.php
@@ -1,3 +1,12 @@
 <?php
-$age = $_POST['age'];
+$agedate = date('Y',strtotime($_POST['age']));
+$todayDate = date("Y");
+$age = $todayDate - $agedate;
+echo "<script>alert(\"$agedate\")</script>";
+echo "<script>alert(\"$age\")</script>";
+if ($age < 18) {
+    echo "<script>alert(\"Vous n'avez pas l'âge requis\")</script>";
+}else {
+    echo "<script>alert(\"Vous pouvez accéder au site\")</script>";
+}
 ?>
